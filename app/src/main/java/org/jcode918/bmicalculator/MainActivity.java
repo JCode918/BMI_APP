11a76a060ccb7679770b2252a72bc981bf61ff94
Willard Joseph <Willard.Joseph26@gmail.com>
2021-11-19 02:08:17 -0500

    Initial Commit Main Functionality of Project Completed.

--- app/src/main/java/org/jcode918/bmicalculator/MainActivity.java
+++ app/src/main/java/org/jcode918/bmicalculator/MainActivity.java
@@ -2,13 +2,121 @@ package org.jcode918.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.jcode918.bmicalculator.model.BMIModel;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText heightEditText;
+    EditText weightEditText;
+    Button calculateBMIButton;
+    TextView bmiResultsTextView;
+    TextView bmiRiskTextView;
+    Button educateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        heightEditText = findViewById(R.id.HeightEditText);
+        weightEditText = findViewById(R.id.WeightEditText);
+        bmiResultsTextView = findViewById(R.id.BmiResultTextView);
+        bmiRiskTextView = findViewById(R.id.BmiMessageTextView);
+        calculateBMIButton = findViewById(R.id.CalculateBMIButton);
+        educateButton = findViewById(R.id.EducateMeButton);
+
+        calculateBMIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callBmiApi();
+            }
+        });
+
+        educateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                educateMe();
+            }
+        });
+    }
+
+    private void educateMe() {
+
+    }
+
+    private void callBmiApi() {
+        Gson gson = new Gson();
+
+        Editable height = heightEditText.getText();
+        Editable weight = weightEditText.getText();
+        // Setting Decimal Format
+        DecimalFormat dFormat = new DecimalFormat("#.00000");
+
+        String URL = "http://webstrar99.fulton.asu.edu/page3/Service1.svc/calculateBMI?height=" + height.toString() + "&weight=" +weight.toString();
+
+        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                BMIModel bmi = gson.fromJson(response, BMIModel.class);
+                // Set Different UI Elements.
+                bmiResultsTextView.setText(String.valueOf(dFormat.format(bmi.getBmi())));
+                setTextColor(bmiResultsTextView, 0);
+
+                bmiRiskTextView.setText(bmi.getRisk());
+                setTextColor(bmiRiskTextView, bmi.getBmi());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+        queue.start();
+    }
+
+    public void setTextColor(TextView tView, double value)
+    {
+      if (value != 0 && value < 18)
+      {
+          tView.setTextColor(Color.BLUE);
+      }
+      else if (value >= 18 && value < 25)
+      {
+          tView.setTextColor(Color.GREEN);
+      }
+      else if (value >= 25 && value <= 30)
+      {
+          tView.setTextColor(Color.rgb(102,0,153));
+      }
+      else if (value > 30)
+      {
+          tView.setTextColor(Color.RED);
+      }
+      else
+      {
+          tView.setTextColor(Color.BLACK);
+      }
     }
 }
---
+++ app/src/main/java/org/jcode918/bmicalculator/model/BMIModel.java
@@ -0,0 +1,51 @@
+package org.jcode918.bmicalculator.model;
+
+import android.graphics.Color;
+
+import java.util.Arrays;
+
+public class BMIModel {
+    private double bmi;
+    private String[] more;
+    private String risk;
+
+    public BMIModel(double bmi, String[] more, String risk) {
+        this.bmi = bmi;
+        this.more = more;
+        this.risk = risk;
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    public String[] getMore() {
+        return more;
+    }
+
+    public void setMore(String[] more) {
+        this.more = more;
+    }
+
+    public String getRisk() {
+        return risk;
+    }
+
+    public void setRisk(String risk) {
+        this.risk = risk;
+    }
+
+    @Override
+    public String toString() {
+        return "BMIModel{" +
+                "bmi=" + bmi +
+                ", more=" + Arrays.toString(more) +
+                ", risk='" + risk + '\'' +
+                '}';
+    }
+
+}
